5195336388ababaa1622cd52d25b5ea93e4f5d24
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-07-23 02:32:06 -0300

    feat: adicionado interceptor no axios para adionar o token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,12 +2,20 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const token = localStorage.getItem("token");
-
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${token}`,
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
